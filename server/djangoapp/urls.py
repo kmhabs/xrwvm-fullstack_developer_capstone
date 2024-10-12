273b1cb4bc49decd185311ce027ec407ae926908
swapnil pawar <swapnil.pawar9870@gmail.com>
2024-10-12 05:25:10 -0400

    Create Django Proxy Services Of Backend APIs

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,4 +1,3 @@
-# Uncomment the imports before you add the code
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
@@ -6,19 +5,16 @@ from . import views
 
 app_name = 'djangoapp'
 urlpatterns = [
-    # # path for registration
     path('register/', views.registration, name='register'),  
-
-    # path for login
     path(route='login', view=views.login_user, name='login'),
-
-    # path for dealer reviews view
-    path('dealer/<int:dealer_id>/reviews/', views.get_dealer_reviews, name='get_dealer_reviews'), 
-
-    # path for add a review view
-    path('add_review/', views.add_review, name='add_review'), 
-    
     path('logout/', views.logout_request, name='logout'), 
-    path('dealer/<int:dealer_id>/', views.get_dealer_details, name='get_dealer_details'),  
+    
+    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
+    path(route='add_review', view=views.add_review, name='add_review'),
+    
+    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
+    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
+    path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
+
     path(route='get_cars', view=views.get_cars, name ='getcars'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
