17b47b4df274371e3838c3df08bcb67aca95701a
kmhabs <abdousidibe412@gmail.com>
2025-02-21 20:44:57 -0500

    update

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -47,3 +47,4 @@ def post_review(data_dict):
     except Exception as e:
         print(f"Network exception occurred: {e}")
         return {"status": 500, "message": "Internal Server Error"}
+        
